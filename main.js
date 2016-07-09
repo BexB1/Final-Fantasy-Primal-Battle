06a033fb0ee3d0d9636f506dee262e3a197f194a
Bex B <bexb1@hotmail.com>
2016-07-09 21:26:31 +0100

    Curative magic implemented and made cheat-free.

--- main.js
+++ main.js
@@ -32,10 +32,9 @@ $('#start').on('click', function (){
 
 
     $('#wrapper').append('<button id="start">START</button>');
+
   })
 });
-
-
 
 
 
@@ -56,7 +55,7 @@ $('#start').on('click', function (){
 
 // The party members constructor object
 
-function PartyMember(name, atk, str, def, mag, hp, defaultHp, mp, turnTaken) {
+function PartyMember(name, atk, str, def, mag, hp, defaultHp, mp) {
   this.name = name;
   this.atk = atk;
   this.str = str,
@@ -65,13 +64,12 @@ function PartyMember(name, atk, str, def, mag, hp, defaultHp, mp, turnTaken) {
   this.hp = hp;
   this.defaultHp = defaultHp;
   this.mp = mp;
-  this.turnTaken = false;
 };
 
 var partyMemberArray = [];
 var actionsChosen = 0;
 
-
+var current_character;
 // the stats for the party members
 
 var fighter = new PartyMember("FIGHTER", 55, 75, 25, 0, 2000, 2000, 0);
@@ -79,7 +77,7 @@ var fighter = new PartyMember("FIGHTER", 55, 75, 25, 0, 2000, 2000, 0);
 partyMemberArray.push(fighter);
 
 var blackMage = new PartyMember("BLACK MAGE", 55, 35, 10, 75, 1000, 1000, 500);
-var whiteMage = new PartyMember("WHITE MAGE", 55, 35, 10, 55, 1000, 1000, 500);
+var whiteMage = new PartyMember("WHITE MAGE", 55, 35, 10, 65, 1000, 1000, 500);
 
 partyMemberArray.push(whiteMage);
 partyMemberArray.push(blackMage);
@@ -107,91 +105,95 @@ function Enemy(name, atk, str, def, mag, hp, mp) {
 var ifrit = new Enemy("IFRIT", 75, 75, 95, 75, 2000, 2000);
 
 
-     var turn = partyMemberArray;
-     var token = 0, state;
+var turn = partyMemberArray;
+var token = 0, state;
 
-     function readToken(token) {  
+function readToken(token) {  
+  state = turn[token];
+  nextAction(state);
+};
 
-       state = turn[token];
-       nextAction(state);
-     };
+readToken(token); 
 
-     readToken(token); 
-
-     function readNextToken() {
-       token++;
-
-       if (token === partyMemberArray.length) {
-         console.log('end of turn');
-         foeAttack();
-         token = 0;
-         return;
-       }
-
-       readToken(token);
-
-     }
-
-// I just need to get this to reset after the enemy's move...
-
-     function nextAction(val){
-       switch (val) {
-
-         case fighter:
-
-            
-            console.log("It's FIGHTER'S turn!");
-            // fighter.turnTaken = true;
-            turnOptions(fighter);
-            $('#attackButton').on('click', function(){
-              // unbind "click" handler
-            $('#attackButton').off('click');
-              readNextToken();
-            });
-
-            break;
+function readNextToken() {
+  token++;
+    if (token === partyMemberArray.length) {
+      console.log('end of turn');
+      foeAttack();
+      token = 0;
+    }
+  readToken(token);
+};
 
 
-         case whiteMage:
+function nextAction(val){
+  switch (val) {
 
-           
-           console.log("It's WHITE MAGE'S turn!");
-           // whiteMage.turnTaken = true;
-             // readNextToken();
-             turnOptions(whiteMage);
-             $('#attackButton').on('click', function(){
-               // unbind "click" handler
-             $('#attackButton').off('click');
-               readNextToken();
-             });
+  case fighter:
 
-             break;
+    current_character = fighter;
+    console.log("It's FIGHTER'S turn!");
 
-        case blackMage:
-      
-           console.log("It's BLACK MAGE'S turn!");
-           // blackMage.turnTaken = true;
-           turnOptions(blackMage);
-           $('#attackButton').on('click', function(){
-             // unbind "click" handler
-             turnOptions(blackMage);
-             $('#attackButton').off('click');
-             readNextToken();
-           });
+    turnOptions(fighter);
 
-           break;
+    $('#attackButton').on('click', function(){
+    // unbind "click" handler
+    $('#attackButton').off('click');
+    readNextToken();
+    });
 
-         default: 
-          console.log("Turn ended.");
+  break;
 
-          break;
-       };
-     }
+
+  case whiteMage:
+
+    current_character = whiteMage;
+    console.log("It's WHITE MAGE'S turn!");
+
+    turnOptions(whiteMage);
+
+    $('#cureBtn').on('click', function(){
+    // unbind "click" handler
+    $('#attackButton').off('click');
+    $('#cureBtn').off('click');
+    readNextToken();
+    });
+
+    $('#attackButton').on('click', function(){
+    // unbind "click" handler
+    $('#cureBtn').off('click');
+    $('#attackButton').off('click');
+    readNextToken();
+    });
+
+  break;
+
+  case blackMage:
+
+  current_character = blackMage;
+  console.log("It's BLACK MAGE'S turn!");
+
+  turnOptions(blackMage);
+
+
+  $('#attackButton').on('click', function(){
+  // unbind "click" handler
+  turnOptions(blackMage);
+  $('#attackButton').off('click');
+  readNextToken();
+  });
+
+  break;
+
+  default: 
+  console.log("Turn ended.");
+  break;
+  };
+  }
 
 // Function for the menu options.
 
   function turnOptions(character) {
-
     // options: Attack, Item
     thisCharacter = character
 
@@ -202,11 +204,17 @@ var ifrit = new Enemy("IFRIT", 75, 75, 95, 75, 2000, 2000);
 
     $('#itemsButton').on('click', function(){
       itemsMenu();
-
-      // will need a function for the using of items.
-
-    actionsChosen++;
     });
+  // will need a function for the using of items.
+
+    // Magic menus
+
+    if (thisCharacter === whiteMage){
+
+      $('#cureBtn').on('click', function(){
+        castCure(fighter);
+      });
+    };
   };
 
 
@@ -265,6 +273,40 @@ function attackCalc(attacker) {
     };
   };
 
+  //  ========= Cure spell ===============
+
+function castCure(target) {
+
+  if (current_character === whiteMage) {
+
+    var target = target;
+
+    var multiplier = function (min,max){
+        return Math.floor(Math.random(1)*(max-min+1)+min);
+    };
+
+    var cureHpUp = (whiteMage.mag * multiplier(3,4) + multiplier(65, 135));
+
+    var targetHealed = target.hp + cureHpUp;
+
+    target.hp = targetHealed;
+
+    console.log("WHITE MAGE healed " + target.name + " for " + cureHpUp + " HP!");
+    console.log(target.name + "'s HP is now: " + target.hp + "/" + "2000");
+
+
+      if (target.hp > target.defaultHp) {
+        target.hp = target.defaultHp;
+      }
+
+    $('#playerCharacter1').html("<p>" + target.hp + "/" + target.defaultHp + "</p>");
+
+    } else {
+      console.log("Only WHM can cure!");
+    }
+
+};
+
 
 // Foe attack round
 
@@ -296,7 +338,7 @@ function foeAttack() {
       $('#playerCharacter1').html("<p>KO!</p>");
       gameOver();
     }
-    readToken();
+
   };
 
 
